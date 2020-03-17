e1052d9e6639c0b955947be5bd4b2f388f242e9a
Júlio César <diolandajr@gmail.com>
2020-03-17 17:04:17 -0300

    New tests added for user model

--- overtime/spec/models/user_spec.rb
+++ overtime/spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(
+      email: "test@test.com", 
+      password: "aaaaaa", 
+      password_confirmation: "aaaaaa",
+      first_name: "Jon",
+      last_name: "Snow"
+    ) 
+  end
+  describe "creation" do
+    it "can be created" do
+      expect(@user).to be_valid
+    end
+
+    it "cannot be created without first_name, last_name" do 
+      @user.first_name = nil
+      @user.last_name = nil
+      expect(@user).to_not be_valid
+    end
+  end
 end
